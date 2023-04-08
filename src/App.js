160a32b932ec03d1c5cb2fc42be9ebdc31679285
Latifano <ikromilatifano76@gmail.com>
2023-04-08 12:49:03 +0700

    move code di App.js ke Home.jsx

--- src/App.js
+++ src/App.js
@@ -1,63 +1,17 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import { getMovieList, searchMovie } from "./api";
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Home from "./pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
-const App = () => {
-  const [popularMovies, setPopularMovies] = useState([]);
-
-  useEffect(() => {
-    getMovieList().then((result) => {
-      setPopularMovies(result);
-    });
-  }, []);
-
-  const search = async (q) => {
-    if (q.length > 3) {
-      const query = await searchMovie(q);
-      setPopularMovies(query.results);
-    }
-  };
-
-  const PopularMovieList = () => {
-    return popularMovies.map((movie, i) => {
-      return (
-        <div className="Movie-wrapper" key={i}>
-          <div className="Movie-title">{movie.title}</div>
-          <img
-            className="Movie-image"
-            src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}
-          />
-          <div className="Movie-date">release : {movie.release_date}</div>
-          <div className="Movie-rate">{movie.vote_average}</div>
-        </div>
-      );
-    });
-  };
-
+function App() {
   return (
     <>
-      {/* <BrowserRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
-      </BrowserRouter> */}
-      <div className="App">
-        <header className="App-header">
-          <h1>Movie List</h1>
-          <input
-            placeholder="What do you want to watch?"
-            className="Movie-search"
-            onChange={({ target }) => search(target.value)}
-          />
-          <div className="Movie-container">
-            <PopularMovieList />
-          </div>
-        </header>
-      </div>
+      </BrowserRouter>
     </>
   );
-};
+}
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,41 @@ import ButtonLogin from "../components/ButtonLogin";
 import ButtonRegis from "../components/ButtonRegis";
 import SearchHome from "../components/SearchHome";
 import backgroundImage from "../styles/image.jpg";
+import { useEffect, useState } from "react";
+import { getMovieList, searchMovie } from "../api";
 
-function Home() {
+const Home = () => {
+  const [popularMovies, setPopularMovies] = useState([]);
+
+  useEffect(() => {
+    getMovieList().then((result) => {
+      setPopularMovies(result);
+    });
+  }, []);
+
+  const search = async (q) => {
+    if (q.length > 3) {
+      const query = await searchMovie(q);
+      setPopularMovies(query.results);
+    }
+  };
+
+  const PopularMovieList = () => {
+    return popularMovies.map((movie, i) => {
+      return (
+        <div className="Movie-wrapper" key={i}>
+          <div className="Movie-title">{movie.title}</div>
+          <img
+            className="Movie-image"
+            src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}
+          />
+          <div className="Movie-date">release : {movie.release_date}</div>
+          <div className="Movie-rate">{movie.vote_average}</div>
+        </div>
+      );
+    });
+  };
+
   return (
     <>
       <div
@@ -54,10 +87,26 @@ function Home() {
 
             <Col></Col>
           </Row>
+
+          <Row>
+            <div className="App">
+              <header className="App-header">
+                <h1>Movie List</h1>
+                <input
+                  placeholder="What do you want to watch?"
+                  className="Movie-search"
+                  onChange={({ target }) => search(target.value)}
+                />
+                <div className="Movie-container">
+                  <PopularMovieList />
+                </div>
+              </header>
+            </div>
+          </Row>
         </Container>
       </div>
     </>
   );
-}
+};
 
 export default Home;
